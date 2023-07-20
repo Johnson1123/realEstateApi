f90bc1a4c5227dca3bda902f3391ebc82c43b250
Johnson1123 <onifadejohnson2017@gmail.com>
2023-07-20 01:33:43 -0700

    first

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from "express";
+const port = 5000;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Sever ready");
+});
+
+app.listen(port, () => console.log(`server running ${port}`));
