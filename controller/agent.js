6d1b10c6ee2eb36f2fea91111af96cc4df1a0d32
Johnson1123 <onifadejohnson2017@gmail.com>
2023-07-21 09:03:29 -0700

    cors and add-agent api

--- controller/agent.js
+++ controller/agent.js
@@ -4,40 +4,40 @@ import asyncHandler from "express-async-handler";
 import Agent from "../model/agent.js";
 
 const addAgent = asyncHandler(async (req, res) => {
-  const {
-    fname,
-    lname,
-    application,
-    phone,
-    address,
-    email,
-    password,
-    experience,
-    qualification,
-    instagram,
-    facebook,
-    twitter,
-    linkedin,
-  } = req.body;
+  // const {
+  //   fname,
+  //   lname,
+  //   application,
+  //   phone,
+  //   address,
+  //   email,
+  //   password,
+  //   experience,
+  //   qualification,
+  //   instagram,
+  //   facebook,
+  //   twitter,
+  //   linkedin,
+  // } = req.body;
 
-  const existAgent = await Agent.findOne({ email });
+  // const existAgent = await Agent.findOne({ email });
 
-  if (existAgent) {
-    res.status(404);
-    throw new Error("Agent already exist");
-  }
+  // if (existAgent) {
+  //   res.status(404);
+  //   throw new Error("Agent already exist");
+  // }
 
-  await Agent.create({
-    name: fname + "" + lname,
-    description: description,
-    phone: phone,
-    address: address,
-    password: password,
-    email: email,
-  });
+  // await Agent.create({
+  //   name: fname + "" + lname,
+  //   description: description,
+  //   phone: phone,
+  //   address: address,
+  //   password: password,
+  //   email: email,
+  // });
+  const values = req.body;
   res.status(201).json({
-    success: true,
-    message: "account created successfully",
+    values,
   });
 });
 
